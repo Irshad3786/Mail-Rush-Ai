1b6f8941c917d29475e50f92c1ec2bd4a77f818d
irshad3786 <irshad9182278505@gmail.com>
2026-01-24 01:04:29 +0530

    feat: enhance dashboard with file generation button and add top templates section; create result page layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -105,9 +105,28 @@ function Dashboard() {
             )}
 
             <div className='flex justify-center items-center pt-2 '>
-              <button className='bg-[#AA3BFF] px-10 py-2 rounded-2xl cursor-pointer font-Roboto_Mono font-bold'>Generate</button>
+              <button className='bg-[#AA3BFF] flex justify-center items-center gap-2 px-10 py-2 rounded-2xl cursor-pointer font-Roboto_Mono font-bold'>Generate <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#000" d="M18 3a4 4 0 0 1 4 4v10a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V7a4 4 0 0 1 4-4zM6 5a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2zm5.412 2.4a.632.632 0 0 1 1.176 0l.316.763a5.46 5.46 0 0 0 2.815 2.908l.896.399c.513.228.514.975 0 1.204l-.95.422a5.45 5.45 0 0 0-2.773 2.813l-.308.707a.633.633 0 0 1-1.168 0l-.308-.707a5.45 5.45 0 0 0-2.773-2.813l-.95-.422c-.513-.229-.513-.976 0-1.204l.896-.399a5.46 5.46 0 0 0 2.814-2.908z"/></svg></button>
+            
             </div>
           </div>
+        </div>
+
+
+        <div>
+          <h1 className='text-center text-white font-Roboto_Mono text-2xl '>Top Templates</h1>
+
+          <div className='flex justify-center items-center gap-8 w-full py-8 flex-wrap'>
+            <div className='w-72 h-56 bg-white rounded-3xl'></div>
+            <div className='w-72 h-56 bg-white rounded-3xl'></div>
+            <div className='w-72 h-56 bg-white rounded-3xl'></div>
+            <div className='w-72 h-56 bg-white rounded-3xl'></div>
+          </div>
+        </div>
+
+        
+
+        <div>
+          
         </div>
       </div>
     </div>
